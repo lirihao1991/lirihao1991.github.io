34fbe9541534d3a06c87a342fa896a5cd63b4b21
lirihao1991 <lirihao1991@users.noreply.github.com>
2017-03-07 16:16:26 +0800

    Create simpleReg.js

---
+++ simpleReg/simpleReg.js
@@ -0,0 +1,106 @@
+function SimpleRegEngine(){
+    this.reg;
+    this.str;
+    this.startChart;
+    this.startStatus;
+    this.dotLength;
+    this.metaSet = {"*":"", ".":""};
+}
+
+
+SimpleRegEngine.prototype.setStartChartAndStatus = function(){
+    var i, reg = this.reg;
+    this.dotLength = 0;
+
+    for (i = 0; i < reg.length; i++){
+        if (this.isMetaChar(reg.charAt(i))){
+            if ((reg.charAt(i)) == "*"){
+                this.reg = this.reg.replace("*","");
+            }
+            else if ((reg.charAt(i)) == "."){
+                this.dotLength += 1;
+                this.reg = this.reg.replace(".","")
+            }
+        }
+        else{
+            this.startChart = reg.charAt(i);
+            break;
+        }
+    }
+}
+
+SimpleRegEngine.prototype.isMetaChar = function(char){
+   return char in this.metaSet ? true : false;
+}
+
+SimpleRegEngine.prototype.trim = function(){
+    var i;
+    for(i = this.reg.length; i >= 0 ; i--){
+        if (this.reg.charAt(i) == '*'){
+            this.reg = this.reg.subString(0, this.reg.length - 1);
+        }
+    }
+}
+
+SimpleRegEngine.prototype.match = function(str,reg){
+
+    var strIndex;
+
+    if (!str || !reg) return false;
+
+    this.reg = reg;
+    this.str = str
+    this.setStartChartAndStatus();
+    
+    if ((!this.reg && this.dotLength <= str.length) || (!this.reg && reg.dotLength == 0)) return true;
+
+    for (strIndex = this.dotLength; strIndex < this.str.length; strIndex++){
+        if (this.str.charAt(strIndex) == this.startChart){
+            var result = this.beginMatch(strIndex);
+            if (result !== false){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+SimpleRegEngine.prototype.beginMatch = function(strIndex){
+    var regIndex, allFlag, i;
+    i = strIndex + 1;
+
+    for (regIndex = 1; regIndex < this.reg.length;regIndex++){
+        switch (this.reg.charAt(regIndex)){
+            case "." : 
+                if (!this.str.charAt(i)){
+                    return false;
+                }
+                i++;
+                break;
+            case "*" :
+                allFlag = true;
+                break;
+            default:
+                if (allFlag == true){
+                    for (i; i < this.str.length; i++){
+                        if (this.str.charAt(i) == this.reg.charAt(regIndex)){
+                            allFlag = false;
+                            i++;
+                            break;
+                        }
+                    }
+
+                    if (allFlag == true){
+                        return false;
+                    }
+                }
+                else{
+                    if (this.str.charAt(i) != this.reg.charAt(regIndex)){
+                        return false
+                    }else{
+                        i++;
+                    }
+                }
+        }
+    }
+}
